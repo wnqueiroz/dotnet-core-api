a1101e8896ba0e5956f79053b00ff8d342be103e
William Queiroz <wnqueiroz@outlook.com>
2021-03-09 15:12:42 -0300

    feat: disable HTTPs redirect

--- Startup.cs
+++ Startup.cs
@@ -47,8 +47,6 @@ namespace csharp_api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "csharp_api v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
