3b7cb7bd80aec6f673087e26aee90ac909222b29
William Queiroz <wnqueiroz@outlook.com>
2021-03-09 11:17:27 -0300

    chore: added Models

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace csharp_api.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres")]
+        public string Title { get; set; }
+    }
+}
